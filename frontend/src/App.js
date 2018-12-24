0a0a8948069bdeb6fc342da842888dbcb9517582
Dominic Roy-Stang <dominicroystang@gmail.com>
2018-12-24 03:17:54 -0500

    Initial commit.

---
+++ frontend/src/App.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <img src={logo} className="App-logo" alt="logo" />
+        <h1>Socket Physics</h1>
+      </div>
+    );
+  }
+}
+
+export default App;
