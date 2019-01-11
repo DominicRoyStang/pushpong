d830550bedfa58de3fa16bea58feaac34b4f42c4
Dominic Roy-Stang <dominicroystang@gmail.com>
2019-01-11 00:32:27 -0500

    Created colors.js.

---
+++ frontend/src/components/Game/utils/colors.js
@@ -0,0 +1,15 @@
+const background = "#ffffff"
+const border = "#2e2b44"
+const paddle1 = "#ffbc42"
+const bumper1 = "#0496ff"
+const paddle2 = "#008000"
+const bumper2 = "#d81159"
+
+export default {
+    background,
+    border,
+    paddle1,
+    bumper1,
+    paddle2,
+    bumper2
+};
