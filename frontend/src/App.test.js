a8b6f0ea7ccc28ea45726b37ac7bf8275a993334
Dominic Roy-Stang <dominicroystang@gmail.com>
2019-01-21 00:39:02 -0500

    Jest (#4)
    * Configured frontend and backend environments for jest testing.

--- frontend/src/App.test.js
+++ frontend/src/App.test.js
@@ -3,7 +3,11 @@ import ReactDOM from 'react-dom';
 import App from './App';
 
 it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
+    const div = document.createElement('div');
+    ReactDOM.render(<App />, div);
+    ReactDOM.unmountComponentAtNode(div);
+});
+
+test("Hello World", () => {
+    expect(true).toBeTruthy();
 });
