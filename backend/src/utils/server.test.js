b3f83f4d4b96f127b67e95be002ba7a49236a8ea
Dominic Roy-Stang <dominicroystang@gmail.com>
2019-01-21 13:33:08 -0500

    Added normalizePort unit test.

--- backend/src/utils/server.test.js
+++ backend/src/utils/server.test.js
@@ -1,3 +1,12 @@
+const {normalizePort} = require("utils/server");
+
 test("Hello World", () => {
     expect(true).toBeTruthy();
+});
+
+test("normalizePort", () => {
+    expect(normalizePort(8080)).toBe(8080);
+    expect(normalizePort(-1)).toBeFalsy();
+    expect(normalizePort("8080")).toBe(8080);
+    expect(normalizePort("named pipe")).toBe("named pipe");
 });
