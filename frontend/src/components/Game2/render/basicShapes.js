d985d904b39fba91909b7cc98483636c8e64c555
Dominic Roy-Stang <dominicroystang@gmail.com>
2019-02-04 16:25:34 -0500

    Updated shape drawing so that it uses the same angle standards as p2.js. Added function to draw convex shapes.

--- frontend/src/components/Game2/render/basicShapes.js
+++ frontend/src/components/Game2/render/basicShapes.js
@@ -10,10 +10,10 @@ const drawCircle = (p, body, color = colors.defaultColor) => {
     const radius = body.shapes[0].radius;
     p.push();
     p.fill(color);
-    p.translate(x, canvasHeight - y);
-    p.rotate(angle);
     // have to do canvas height - y because p5 (canvas) uses top left corner as (0,0)
     // whereas p2 (physics) uses the bottom left corner as (0,0)
+    p.translate(x, canvasHeight - y);
+    p.rotate(-angle);
     p.circle(0, 0, radius);
     p.pop();
 };
@@ -28,15 +28,38 @@ const drawRectangle = (p, body, color = colors.defaultColor) => {
     const height = body.shapes[0].height;
     p.push();
     p.fill(color);
-    p.translate(x, canvasHeight - y);
-    p.rotate(angle);
     // have to do canvas height - y because p5 (canvas) uses top left corner as (0,0)
     // whereas p2 (physics) uses the bottom left corner as (0,0)
+    p.translate(x, canvasHeight - y);
+    p.rotate(-angle);
     p.rect(0, 0, width, height);
+    p.pop();
+}
+
+/*
+ * Takes a p5 instance and a p2 convex body, and draws it to the canvas in the desired color.
+ */
+const drawConvex = (p, body, color = colors.defaultColor) => {
+    const [x, y] = body.interpolatedPosition;
+    const angle = body.interpolatedAngle;
+    const vertices = body.shapes[0].vertices;
+    p.push();
+    p.fill(color);
+    // have to do canvas height - y because p5 (canvas) uses top left corner as (0,0)
+    // whereas p2 (physics) uses the bottom left corner as (0,0)
+    p.translate(x, canvasHeight - y);
+    p.rotate(-angle);
+    p.beginShape();
+    for (const vertex of vertices) {
+        const [vx, vy] = vertex;
+        p.vertex(vx, -vy);
+    }
+    p.endShape(p.CLOSE);
     p.pop();
 }
 
 export {
     drawCircle,
+    drawConvex,
     drawRectangle
 };
